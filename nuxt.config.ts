af22bdbfb21c806b9632dc3849075ba14556e10d
Hoàng Anh Lâm <hoanglam.bk57@gmail.com>
2023-05-14 15:30:12 +0700

    add google tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,17 @@ export default defineNuxtConfig({
         {name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'},
       ],
       script: [
-        {src: 'https://feedback.fish/ff.js?pid=8ef1a83add0137', defer: true}
+        {src: 'https://feedback.fish/ff.js?pid=8ef1a83add0137', defer: true},
+        {
+          hid: 'gtmHead',
+          innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-55P679J');
+          `,
+        },
       ]
     },
   },
