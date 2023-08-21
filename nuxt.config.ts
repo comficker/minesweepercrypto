52681be39e8f76c5eb418a89a2a8ea9f5d1f7266
Hoàng Anh Lâm <comficker@gmail.com>
2023-08-21 14:27:24 +0700

    OK

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
       meta: [
         {name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes'},
         {name: 'description', content: appDescription},
-        {name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'},
+        // {name: 'apple-mobile-web-app-status-bar-style', content: 'default'},
       ],
       script: [
         {
