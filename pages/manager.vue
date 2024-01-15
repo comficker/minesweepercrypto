a1ad9f04dc8f289d8d0562f91e1f357d60ed014e
comficker <comficker@gmail.com>
2024-01-15 23:18:02 +0700

    OK

--- pages/manager.vue
+++ pages/manager.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="space-y-3 w-full max-w-lg mx-auto">
     <client-only>
-      <div class="space-y-2 min-h-md flex flex-col items-center justify-center">
-        <div class="flex items-center flex-col gap-3 justify-center">
+      <div class="space-y-6 min-h-md flex flex-col items-center justify-center">
+        <div class="flex items-center flex-col gap-6 justify-center">
           <div class="text-xs uppercase font-bold">Balance</div>
-          <div v-if="userStore.isLogged" class="flex gap-2 items-center text-6xl">
+          <div v-if="userStore.isLogged" class="flex gap-2 items-center text-6xl font-mono">
             <img src="/coin.png" class="w-5 h-5" alt="Coin"/>
             <b>{{ userStore.balance }}</b>
           </div>
         </div>
         <div class="flex justify-center gap-6">
-          <div class="bg-blue-500 cursor-pointer rounded p-2 px-6 text-white">Deposit</div>
-          <div class="cursor-pointer rounded p-2 px-6">Withdraw</div>
+          <div class="min-w-32 text-center bg-blue-500 cursor-pointer rounded p-2 px-6 text-white">Deposit</div>
+          <div class="min-w-32 text-center cursor-pointer rounded p-2 px-6 border">Withdraw</div>
+        </div>
+        <div class="flex justify-center">
+          <div class="rounded text-xs p-1 px-3 shadow bg-amber-2">Coming soon!</div>
         </div>
       </div>
       <invite-form v-if="userStore.isLogged" :show-history="false"/>
